1db83904989745cabc13f70b0edea81e6e33306d
pratham16-ux <prathamkattimani3353@gmail.com>
2026-06-19 14:12:48 +0530

    update

--- contact.js
+++ contact.js
@@ -408,23 +408,5 @@
     });
   }
 
-  // Load Leaflet CSS + JS dynamically so nothing extra is needed in <head>
-  (function loadLeaflet() {
-    if (!document.getElementById("leafletMap")) return; // map div not on this page
-
-    var link = document.createElement("link");
-    link.rel  = "stylesheet";
-    link.href = "https://unpkg.com/leaflet@1.9.4/dist/leaflet.css";
-    link.integrity = "sha256-p4NxAoJBhIIN+hmNHrzRCf9tD/miZyoHS5obTRR9BMY=";
-    link.crossOrigin = "";
-    document.head.appendChild(link);
-
-    var script = document.createElement("script");
-    script.src = "https://unpkg.com/leaflet@1.9.4/dist/leaflet.js";
-    script.integrity = "sha256-20nQCchB9co0qIjJZRGuk2/Z9VM+kNiyxNV/XN/sp0=";
-    script.crossOrigin = "";
-    script.onload = initLeafletMap;
-    document.head.appendChild(script);
-  }());
 
 })();
